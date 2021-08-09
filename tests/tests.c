d3f6814536ccdd75f8cb9f879b285087df6a88b4
Henrique Rocha <henriquerocha@tutanota.com>
2021-08-09 09:48:20 -0400

    feat: add new tests to main function

--- tests/tests.c
+++ tests/tests.c
@@ -15,6 +15,7 @@ static void print_result(bool check, int number)
 
 int	main(void)
 {
+	printf("\nis_map Function: ");
 	print_result(is_map_with_valid_name(), 0);
 	print_result(is_map_with_invalid_name_1(), 1);
 	print_result(is_map_with_invalid_name_2(), 2);
@@ -26,5 +27,19 @@ int	main(void)
 	print_result(is_map_with_invalid_name_8(), 8);
 	print_result(is_map_with_invalid_name_9(), 9);
 	printf("\n");
+
+	printf("\nrow_validator Function: ");
+	print_result(row_validator_with_valid_row_1(), 0);
+	print_result(row_validator_with_valid_row_2(), 1);
+	print_result(row_validator_with_valid_row_3(), 2);
+	print_result(row_validator_with_invalid_row_1(), 3);
+	print_result(row_validator_with_invalid_row_2(), 4);
+	print_result(row_validator_with_invalid_row_3(), 5);
+	print_result(row_validator_with_invalid_row_4(), 6);
+	print_result(row_validator_with_invalid_row_5(), 7);
+	print_result(row_validator_with_invalid_row_6(), 8);
+	print_result(row_validator_with_invalid_row_7(), 9);
+	printf("\n");
+
 	return (err);
 }
