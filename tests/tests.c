8159d4da396a2dd398d1e0785beaece27a13c86d
Henrique Rocha <henriquerocha@tutanota.com>
2021-08-12 14:45:06 -0400

    fix: validate_row unit tests

--- tests/tests.c
+++ tests/tests.c
@@ -28,17 +28,14 @@ int	main(void)
 	print_result(is_map_with_invalid_name_9(), 9);
 	printf("\n");
 
-	printf("\nrow_validator Function: ");
-	print_result(row_validator_with_valid_row_1(), 0);
-	print_result(row_validator_with_valid_row_2(), 1);
-	print_result(row_validator_with_valid_row_3(), 2);
-	print_result(row_validator_with_invalid_row_1(), 3);
-	print_result(row_validator_with_invalid_row_2(), 4);
-	print_result(row_validator_with_invalid_row_3(), 5);
-	print_result(row_validator_with_invalid_row_4(), 6);
-	print_result(row_validator_with_invalid_row_5(), 7);
-	print_result(row_validator_with_invalid_row_6(), 8);
-	print_result(row_validator_with_invalid_row_7(), 9);
+	printf("\nvalidate_row Function: ");
+	print_result(validate_row_with_valid_row_1(), 0);
+	print_result(validate_row_with_valid_row_2(), 1);
+	print_result(validate_row_with_valid_row_3(), 2);
+	print_result(validate_row_with_invalid_row_1(), 3);
+	print_result(validate_row_with_invalid_row_2(), 4);
+	print_result(validate_row_with_invalid_row_3(), 5);
+	print_result(validate_row_with_invalid_row_4(), 6);
 	printf("\n");
 
 	return (err);
