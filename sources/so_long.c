94bf04e06306158defe45eff662d8d6a250ed974
Henrique Rocha <henriquerocha@tutanota.com>
2021-08-12 15:33:47 -0400

    refactor: the main function

--- sources/so_long.c
+++ sources/so_long.c
@@ -2,7 +2,28 @@
 #include "mlx.h"
 #include "so_long.h"
 
-int	main(void)
+int	main(int argc, char *argv[])
 {
+	t_map	*game_map;
+	int		map_fd;
+
+	if (argc != 2)
+		error("main");
+	if (!is_map(argv[1]))
+		error("is_map");
+	map_fd = open(argv[1], O_RDONLY);
+	if (map_fd == -1)
+		error("open");
+	game_map = parse_map(map_fd);
+
+
+	// render_game
+	// start_game
+	// handle_game
+	// end_game
+	//  /* void	(*free_ptr)(void *); */
+	//  /* free_ptr = &free; */
+	//  /* ft_lstclear(&game_map->rows, free_ptr); */
+	//  /* free(game_map); */
 	return (0);
 }
