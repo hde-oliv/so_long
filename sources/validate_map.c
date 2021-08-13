cd9a4f8d14b94b5f81feab07b5b5253d083310d6
Henrique Rocha <henriquerocha@tutanota.com>
2021-08-13 15:14:48 -0400

    refactor: change count_exits to static

--- sources/validate_map.c
+++ sources/validate_map.c
@@ -1,6 +1,19 @@
 #include "so_long.h"
 #include "libft.h"
 
+static int	count_exits(char *row)
+{
+	int	answer;
+
+	while (*row)
+	{
+		if (*row == 'E')
+			answer++;
+		row++;
+	}
+	return (answer);
+}
+
 bool	validate_map(t_list *rows)
 {
 	static int	collectibles;
@@ -16,23 +29,10 @@ bool	validate_map(t_list *rows)
 		tmp_size = ft_strlen(rows->content);
 		if (tmp_size != row_size)
 			return (false);
-		exits += count_exits(rows->content)
+		exits += count_exits(rows->content);
 		rows = rows->next;
 	}
 	if (!collectibles || !exits || !players)
 		return (false);
 	return (true);
-}
-
-int	count_exits(char *row)
-{
-	int	answer;
-
-	while (*row)
-	{
-		if (*row == 'E')
-			answer++;
-		row++;
-	}
-	return (answer);
 }
